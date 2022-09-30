f0e1ec1b7e37ab0cbfb4d3fbdbea97b1a6e5149b
lass1528 <107149493+lass1528@users.noreply.github.com>
2022-09-30 15:56:54 -0400

    Add files via upload
    pagina web de voluntariados y campers

---
+++ Guardar.php
@@ -0,0 +1,45 @@
+﻿<?php
+        //conectamos con el servidor 
+         =$conectar=mysql_connect('databases.000webhost.com','id18838052_gustavo','$h+@0UX4(p-S=t_/')
+        //verificamos la conexion
+        if(!$conectar){
+            echo ''No se pudo conectar con el servidor ''
+        }else{
+
+            $base=mysql_select_db( 'id18838052_misegundabase' );
+            if(!$base){
+                echo''No se encontro la base de datos'';
+            }
+        }
+        //recuperar las variables
+        $nombre=$_POST[ ' nombre ' ];
+        $email=$_POST[ ' email ' ];
+		$telf=$_POST[ ' telf ' ];
+		$Cantidad=$_POST[' Cantidad ' ];
+		$fechahora_usuario=$_POST[ ' fechahora_usuario ' ]
+		$list=$_POST [ ' list ' ]
+		$Ref!!=$_POST [ ' Ref!! ' ]
+		$output=$_POST [ ' output ' ]
+		$file=$_POST [ ' file ']
+		$file=$_POST [ ' file ']
+		
+        //hacemos la sentencia sql
+        $sql=''INSERT INTO datos  VALUES ( '$nombre',
+                                           '$email',
+                                           '$telf',
+										   '$Cantidad',
+										   '$Fecha_usuario',
+										   'list',
+										   'Ref!!',
+										   'output',
+										   'file',
+										   'file')'';
+        //ejecutamos la sentencia sql
+        $ejecutar=mysql_query($sql);
+        //verificamos la ejecucion
+        if(!$ejecutar){
+            echo  ''Ups... ¡Hubo un error! ";
+        }else{
+            echo "Datos guardados correctamente<br><a href= ' Formulario.html ' >Volver</a>'';
+        }
+?>
---
+++ table1.php
@@ -0,0 +1,23 @@
+<HTML>
+<HEAD>
+ <TITLE>New Document</TITLE>
+</HEAD>
+<BODY>
+<?
+  <TABLE ID=TAB1 border=8><CAPTION>TABLE-1</CAPTION>
+<THEAD>
+<TH>NOMBRES</TH><TH>EMAIL</TH><TH>MOVIL</TH><TH>PERMANENCIA</TH><TH>FECHA</TH><TH>TIPO</TH><TH>REFERENCIA</TH><TH>COLOR</TH><TH>COMENTARIO</TH><TH>ARCHIVO</TH><TH>ARCHIVO</TH>
+</THEAD>
+<TBODY id='body1'>
+<TR><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD></TR>
+<TR><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD></TR>
+<TR><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD></TR>
+<TR><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD><TD></TD></TR>
+</TBODY>
+<TFOOT>
+<TR></TR><TR></TR><TR></TR><TR></TR><TR></TR><TR></TR><TR></TR><TR></TR><TR></TR><TR></TR><TR></TR><TR></TR>
+</TFOOT>
+</TABLE>
+?>
+</BODY>
+</HTML>
